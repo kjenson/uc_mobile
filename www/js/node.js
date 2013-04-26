a04accbce8ef3b4278b44288c478d48900def9dd
Dax Panganiban <p_dax2003@yahoo.com>
2013-04-26 17:53:12 +0800

    Testing audio

--- www/js/node.js
+++ www/js/node.js
@@ -11,17 +11,18 @@ $('#drupalgap_node').on('pageshow', function(){
 			$('#node_title').html(node.title);
 			$('#node_content').html(node.content);
             
-            if(node.field_dreamland_stream.length > 0 && drupalgap.user.uid) {                
-                if(node.field_dreamland_stream[0].value) {
-                    var src = node.field_dreamland_stream[0].value;
+            if(node.field_dreamland_mp3 != null && node.field_dreamland_mp3[0].length > 0 && drupalgap.user.uid) {
+                if(node.field_dreamland_mp3[0].filepath) {
+                    var src = sitename + node.field_dreamland_mp3[0].filepath;
                     
                     initAudioPlayer(src);                    
                 }
             }
-            
+            /*
             if(node.field_dreamland_mp3 != null &&  node.field_dreamland_mp3.length > 0 && drupalgap.user.uid) {
                 if(node.field_dreamland_mp3[0].filepath) {
-                    $('#high-quality-mp3').attr('href', sitename + node.field_dreamland_mp3[0].filepath);
+                    $('#high-quality-mp3').attr('download-url', sitename + node.field_dreamland_mp3[0].filepath);
+                    $('#high-quality-mp3').attr('filename', node.field_dreamland_mp3[0].filename);
                 } else {
                     $('#high-quality-mp3').hide();
                 }
@@ -31,7 +32,8 @@ $('#drupalgap_node').on('pageshow', function(){
 
             if(node.field_dreamland_mp3_low != null && node.field_dreamland_mp3_low.length > 0 && drupalgap.user.uid) {
                 if(node.field_dreamland_mp3_low[0].filepath) {
-                    $('#low-quality-mp3').attr('href', sitename + node.field_dreamland_mp3_low[0].filepath);                    
+                    $('#low-quality-mp3').attr('download-url', sitename + node.field_dreamland_mp3_low[0].filepath);
+                    $('#low-quality-mp3').attr('filename', node.field_dreamland_mp3_low[0].filename);
                 } else {
                     $('#low-quality-mp3').hide();
                 }
@@ -39,10 +41,48 @@ $('#drupalgap_node').on('pageshow', function(){
                 $('#low-quality-mp3').hide();
             }
 
-            $('#node-mp3-files').show();
+            $('#node-mp3-files').show(); */
 		}
 	});
+    /*
+    $('.download-file').live('click', function () {
+        var src = $(this).attr('download-url');
+        var filename = $(this).attr('filename');
+
+        window.requestFileSystem(
+                 LocalFileSystem.PERSISTENT, 0,
+                 function onFileSystemSuccess(fileSystem) {
+                 fileSystem.root.getFile(
+                             "dummy.html", {create: true, exclusive: false},
+                             function gotFileEntry(fileEntry){
+                             var sPath = fileEntry.fullPath.replace("dummy.html","");
+                             var fileTransfer = new FileTransfer();
+                             fileEntry.remove();
+
+                             fileTransfer.download(
+                                       src,
+                                       sPath + filename,
+                                       function(theFile) {
+                                           console.log("download complete: " + theFile.toURI());
+                                           //showLink(theFile.toURI());
+                                       },
+                                       function(error) {
+                                           console.log("download error source " + error.source);
+                                           console.log("download error target " + error.target);
+                                           console.log("upload error code: " + error.code);
+                                       }
+                                       );
+                             },
+                             fail);
+                 },
+                 fail);
+
+    }); */
 });
+
+function fail(evt) {
+    console.log(evt.target.error.code);
+}
 
 function initAudioPlayer(src) {
     $("#playaudio").live('tap', function() {        
@@ -55,5 +95,8 @@ function initAudioPlayer(src) {
 
     $("#stopaudio").live('tap', function() {
         stopAudio();
-    });    
+    });
+
+    $('#audio-player').show();
 }
+
